e9bf870def112e4ed8f73292d35396ef34e9e188
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-07-12 14:47:27 -0300

    Create off_calcpi.c

---
+++ src/off_calcpi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <omp.h>
+const long num_passos = 10000000000;
+
+int main(int argc, char *argv[]) { /* off_calcpi.c  */
+double passo = 1.0 / (double) num_passos;
+double inicio, fim, soma = 0.0; 
+
+   inicio = omp_get_wtime();
+   // Offloading com OpenMP para o acelerador (GPU)
+   #pragma omp target teams distribute parallel for reduction(+: soma) map(tofrom: soma) device(1)
+   for (long int i = 0; i < num_passos; i++) {
+      double x = (i + 0.5) * passo;
+      soma += 4.0 / (1.0 + x * x);
+   }
+   double pi = soma * passo; // calcula o valor final de Pi
+   fim = omp_get_wtime();
+   printf("Valor calculado de Pi: %2.15f em %f segundos.\n", pi, fim-inicio);
+   return(0);
+}
