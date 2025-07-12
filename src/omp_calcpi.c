392c3f96ce94050c76c8709c0bca18338e4601f3
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-07-12 14:45:50 -0300

    Update omp_calcpi.c

--- src/omp_calcpi.c
+++ src/omp_calcpi.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <omp.h>
-#define N 10000000000
+const long num_passos = 10000000000;
 
 int main(int argc, char *argv[]) { /* omp_calcpi.c  */
-double inicio, fim, pi = 0.0f; 
+double passo = 1.0 / (double) num_passos;
+double inicio, fim, soma = 0.0; 
+
    inicio = omp_get_wtime();
-   #pragma omp parallel for reduction(+: pi)
-    for (long i = 0; i < N; i++) {
-        double t=(double) ((i+0.5)/N);
-        pi += 4.0/(1.0+t*t);
+   #pragma omp parallel for reduction(+: soma)
+   for (long int i = 0; i < num_passos; i++) {
+      double x = (i + 0.5) * passo;
+      soma += 4.0 / (1.0 + x * x);
    }
-    fim = omp_get_wtime();
-    printf("O valor de pi é: %1.9f\n",pi/N);
-    printf("O tempo de execução é %1.3f segundos.\n",fim-inicio);
-    return(0);
+   double pi = soma * passo;
+   fim = omp_get_wtime();
+   printf("Valor calculado de Pi: %2.15f em %f segundos.\n", pi, fim-inicio);
+   return(0);
 }
