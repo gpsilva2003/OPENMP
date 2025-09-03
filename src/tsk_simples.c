b0e2b06d298555227538bcb401839d1e12245e24
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-09-03 20:33:04 -0300

    Implement OpenMP parallel tasks in tsk_simples.c

---
+++ src/tsk_simples.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char* argv[]) {  /*  tsk_simples */
+#pragma omp parallel num_threads(4)
+{
+    int tid=omp_get_thread_num();
+    #pragma omp single
+    {
+        printf("Thread %d: Esta thread está criando tarefas para as outras threads.\n", tid);
+        for (int i = 0; i < 5; i++) {
+    // Criando múltiplas tarefas
+            #pragma omp task
+            {
+    // Cada tarefa é executada por qualquer thread disponível
+                int exec_id = omp_get_thread_num();
+                printf("Thread %d: Executando tarefa %d criada pela thread %d.\n", exec_id, i, tid);
+                // Simulando trabalho
+                for (int j = 0; j < 10000000; j++); 
+            }
+        }
+        printf("Thread %d: Todas as tarefas foram criadas.\n", tid);
+    } // Barreira implícita ao final da região single
+}    // onde as threads esperam todas tarefas serem concluídas
+    printf("Programa finalizado.\n");
+    return 0;
+}
